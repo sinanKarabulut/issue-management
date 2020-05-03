6de3e3a7ae5fc0162d3bd38a7392b8e3055331fe
skbt <sinankarabulut87@gmail.com>
2020-05-03 13:43:08 +0300

    7.Exception_handle

---
+++ src/main/java/com/skbt/issuemanagement/advice/ExceptionResponse.java
@@ -0,0 +1,15 @@
+package com.skbt.issuemanagement.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor // instance yaratmak istenebilir
+public class ExceptionResponse {
+    private Date date;
+    private String message;
+}
